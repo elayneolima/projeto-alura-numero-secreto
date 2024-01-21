c974dd19dc5200b96feb34e71c1ab797df7a9833
elayneolima <elayne04lima@gmail.com>
2024-01-21 19:31:46 -0300

    adicionando arquivo

---
+++ app.js
@@ -0,0 +1,20 @@
+alert('Bem vindo(a) ao jogo do Número Secreto'); 
+
+let numeroSecreto  = 10; 
+let chuteUsuario 
+
+
+while (chuteUsuario != numeroSecreto) {
+    chuteUsuario = prompt('Insira o número secreto: ');
+    if (numeroSecreto == chuteUsuario){
+        alert('Parabéns, parece que você acertou!');
+    } else{
+        if (chuteUsuario > numeroSecreto){
+            alert(`você digitou ${chuteUsuario}, O numero secreto é menor!`);
+        } else{
+                alert(`você digitou ${chuteUsuario}, O numero secreto é maior!`)
+            }
+    }
+
+}
+
