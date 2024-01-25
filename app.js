311263df188f4a5fe713af594e7c1bd5fb337ec8
elayneolima <elayne04lima@gmail.com>
2024-01-25 12:52:59 -0300

    adicionando arquivo de melhorias e ajuste para exibir tentativas no acerto

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ alert('Bem vindo(a) ao jogo do Número Secreto');
 
 let numeroSecreto  = 10; 
 let chuteUsuario;
+let tentativas = 1;
 
-
+// Enquanto chute não for igual ao numero secreto
 while (chuteUsuario != numeroSecreto) {
     chuteUsuario = prompt('Insira o número secreto: ');
+    //se chute for igual ao numero secreto 
     if (numeroSecreto == chuteUsuario){
-        alert('Parabéns, parece que você acertou!');
+        alert(`Parabéns, parece que você acertou o número secreto com ${tentativas} tentativas!`);
     } else{
         if (chuteUsuario > numeroSecreto){
             alert(`você digitou ${chuteUsuario}, O numero secreto é menor!`);
         } else{
                 alert(`você digitou ${chuteUsuario}, O numero secreto é maior!`);
-             }
+        }
     }
-
 }
-
